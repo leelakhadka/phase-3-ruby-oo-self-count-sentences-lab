b7b18b52f220f90b126f7e3bdcdd75905fcf38ce
leela khadka <khadkaleela3@gmail.com>
2022-09-25 10:52:32 -0700

    test passed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    #Example here self = "Hi, my name is Sophie."
+     self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    ary = self.split(".", 4)
+    ary.count
   end
 end
